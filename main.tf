654cfd41a6ce51d9f322a2c933faf1a5952e833f
kusumsiri <kusumsiri@gmail.com>
2023-03-16 16:09:51 +0530

    Adding main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicrop/aws"
+       version = "~> 4.16"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "myDynamo_table" {
+    name    = "my-dynamo-table-example"
+    hash_key    = "email"
+    billing_mode    = "PAY_PER_REQUEST"
+}
